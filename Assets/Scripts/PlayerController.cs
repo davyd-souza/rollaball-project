38e5cbf30cee90b6725ecd52e5c9ee5dd961597f
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2020-10-15 22:29:16 -0300

    Created a basic UI; Added a counter for collectables; Added Win text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,30 @@
-﻿using System.Collections;
+﻿// Importing Namespaces
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    // Declaring variables
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue) 
@@ -22,6 +33,15 @@ public class PlayerController : MonoBehaviour
         
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate() 
@@ -35,6 +55,8 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.CompareTag("PickUp")) 
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
     }
 }
